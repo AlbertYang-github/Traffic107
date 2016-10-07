57c01d4691bad1f74da3b8be5531752954c8c2f8
Albert Yang <albertyang_mail@yeah.net>
2016-10-07 05:19:57 +0000

    修改登录模式

--- app/src/main/java/com/yohann/traffic107/common/activity/LoginActivity.java
+++ app/src/main/java/com/yohann/traffic107/common/activity/LoginActivity.java
@@ -2,13 +2,14 @@ package com.yohann.traffic107.common.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.yohann.traffic107.R;
 import com.yohann.traffic107.common.Constants.Variable;
@@ -19,7 +20,6 @@ import com.yohann.traffic107.user.activity.RegisterActivity;
 import com.yohann.traffic107.utils.BmobUtils;
 import com.yohann.traffic107.utils.ViewUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
@@ -32,15 +32,16 @@ import cn.bmob.v3.listener.FindListener;
 public class LoginActivity extends BaseActivity {
     private static final String TAG = "LoginActivityInfo";
 
-    private ViewPager vp;
-    private EditText etUsername_user;
-    private EditText etPassword_user;
-    private EditText etUsername_root;
-    private EditText etPassword_root;
+    private EditText etUsername;
+    private EditText etPassword;
+    private CheckBox cbUserType;
+    private ImageView ivLogin;
+    private TextView tvRegister;
+    private String username;
+    private String password;
+    private ProgressBar pbLogin;
     private User user;
     private Root root;
-    private ProgressBar pb_user;
-    private ProgressBar pb_root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,56 +52,57 @@ public class LoginActivity extends BaseActivity {
     }
 
     private void init() {
-        vp = (ViewPager) findViewById(R.id.vp);
+        etUsername = (EditText) findViewById(R.id.et_username);
+        etPassword = (EditText) findViewById(R.id.et_password);
+        cbUserType = (CheckBox) findViewById(R.id.cb_user_type);
+        ivLogin = (ImageView) findViewById(R.id.iv_login);
+        tvRegister = (TextView) findViewById(R.id.tv_register);
+        pbLogin = (ProgressBar) findViewById(R.id.pb_login);
 
-        View viewUser = View.inflate(this, R.layout.login_user, null);
-        View viewRoot = View.inflate(this, R.layout.login_root, null);
-
-        etUsername_user = (EditText) viewUser.findViewById(R.id.et_username_user);
-        etUsername_root = (EditText) viewRoot.findViewById(R.id.et_username_root);
-        etPassword_user = (EditText) viewUser.findViewById(R.id.et_password_user);
-        etPassword_root = (EditText) viewRoot.findViewById(R.id.et_password_root);
-        pb_user = (ProgressBar) viewUser.findViewById(R.id.pb_user);
-        pb_root = (ProgressBar) viewRoot.findViewById(R.id.pb_root);
-
-        final ArrayList<View> viewList = new ArrayList<>();
-        viewList.add(viewUser);
-        viewList.add(viewRoot);
-
-        PagerAdapter pagerAdapter = new PagerAdapter() {
+        //注册
+        tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public int getCount() {
-                return viewList.size();
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
             }
+        });
 
+        //登录
+        ivLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return view == object;
+            public void onClick(View v) {
+                if (cbUserType.isChecked()) {
+                    //登录工作端
+                    rootLogin();
+                } else {
+                    //登录普通用户端
+                    userLogin();
+                }
             }
+        });
 
+        cbUserType.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                container.addView(viewList.get(position));
-                return viewList.get(position);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    tvRegister.setVisibility(View.INVISIBLE);
+                } else {
+                    tvRegister.setVisibility(View.VISIBLE);
+                }
             }
-
-            @Override
-            public void destroyItem(ViewGroup container, int position, Object object) {
-                container.removeView(viewList.get(position));
-            }
-        };
-
-        vp.setAdapter(pagerAdapter);
+        });
     }
 
-    public void userLogin(View view) {
-        final String username = etUsername_user.getText().toString();
-        final String password = etPassword_user.getText().toString();
+    public void userLogin() {
+
+        username = etUsername.getText().toString();
+        password = etPassword.getText().toString();
 
         if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
             return;
         } else {
-            pb_user.setVisibility(View.VISIBLE);
+            pbLogin.setVisibility(View.VISIBLE);
             user = new User();
             user.setUsername(username);
             user.setPassword(password);
@@ -131,18 +133,18 @@ public class LoginActivity extends BaseActivity {
                                 } else {
                                     ViewUtils.show(LoginActivity.this, "登录失败");
                                 }
-                                pb_user.post(new Runnable() {
+                                pbLogin.post(new Runnable() {
                                     @Override
                                     public void run() {
-                                        pb_user.setVisibility(View.INVISIBLE);
+                                        pbLogin.setVisibility(View.INVISIBLE);
                                     }
                                 });
                             } else {
                                 ViewUtils.show(LoginActivity.this, "异常 " + e.getErrorCode());
-                                pb_user.post(new Runnable() {
+                                pbLogin.post(new Runnable() {
                                     @Override
                                     public void run() {
-                                        pb_user.setVisibility(View.INVISIBLE);
+                                        pbLogin.setVisibility(View.INVISIBLE);
                                     }
                                 });
                             }
@@ -153,19 +155,14 @@ public class LoginActivity extends BaseActivity {
         }
     }
 
-    public void userRegister(View view) {
-        startActivity(new Intent(this, RegisterActivity.class));
-        finish();
-    }
-
-    public void rootLogin(View view) {
-        final String username = etUsername_root.getText().toString();
-        final String password = etPassword_root.getText().toString();
+    public void rootLogin() {
+        username = etUsername.getText().toString();
+        password = etPassword.getText().toString();
 
         if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
             return;
         } else {
-            pb_root.setVisibility(View.VISIBLE);
+            pbLogin.setVisibility(View.VISIBLE);
             root = new Root();
             root.setUsername(username);
             root.setPassword(password);
@@ -195,18 +192,18 @@ public class LoginActivity extends BaseActivity {
                                 } else {
                                     ViewUtils.show(LoginActivity.this, "登录失败");
                                 }
-                                pb_root.post(new Runnable() {
+                                pbLogin.post(new Runnable() {
                                     @Override
                                     public void run() {
-                                        pb_root.setVisibility(View.INVISIBLE);
+                                        pbLogin.setVisibility(View.INVISIBLE);
                                     }
                                 });
                             } else {
                                 ViewUtils.show(LoginActivity.this, "异常 " + e.getErrorCode());
-                                pb_root.post(new Runnable() {
+                                pbLogin.post(new Runnable() {
                                     @Override
                                     public void run() {
-                                        pb_root.setVisibility(View.INVISIBLE);
+                                        pbLogin.setVisibility(View.INVISIBLE);
                                     }
                                 });
                             }
